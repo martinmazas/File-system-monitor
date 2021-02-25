d08507f37e924bcf09d347b6da9c74d022a395df
martinmazas <mazasmartin@gmail.com>
2021-02-25 15:30:06 +0200

    poll, read and events added

--- myFileSystemMonitor.c
+++ myFileSystemMonitor.c
@@ -11,17 +11,16 @@
 #include "libcli.h"
 #include <unistd.h>
 #include <poll.h>
-
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <execinfo.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <fcntl.h>
-#include <sys/time.h>
-#include <memory.h>
-#include <ifaddrs.h>
-#include <net/if.h>
-#include <stdarg.h>
+#include <time.h>
+#include <stdbool.h>
 
 #define PORT 5000		//Netcat server port
 #define BT_BUF_SIZE 1024
@@ -35,6 +34,127 @@ int telnetListen = 1;
 int backTrace = 0;
 sem_t semaphore;
 int listenSkt;
+
+void sendToServer(char *time, char *access, char *name)
+{
+    int sock, nsent;
+    char sendPacket[2048];
+    memset(sendPacket, 0, sizeof(sendPacket));
+
+    struct sockaddr_in s = {0};
+    s.sin_family = AF_INET;
+    s.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, ip, &s.sin_addr.s_addr) <=0)
+    {
+        perror("\nInvalid address/Address not supported");
+        exit(1);
+    }
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if(connect(sock, (struct sockaddr*)&s, sizeof(s)) < 0)
+    {
+        perror("connect");
+        exit(1);
+    }
+
+    strcpy(sendPacket, "\nFILE ACCESSED: ");
+    strcat(sendPacket, name);
+    strcat(sendPacket, "\nACCESS: ");
+    strcat(sendPacket, access);
+    strcat(sendPacket, "\nTIME OF ACCESS: ");
+    strcat(sendPacket, time);
+    strcat(sendPacket, "\n");
+    strcat(sendPacket, "\0");
+
+    if((nsent = send(sock, sendPacket, strlen(sendPacket), 0)) < 0)
+    {
+        perror("recv");
+        exit(1);
+    }
+
+    close(sock);
+    exit(0);
+}
+
+static void handle_events(int fd, int wd, int fdHTML)
+{
+    char buffer[4096] __attribute__((aligned(__alignof__(struct inotify_event))));
+    const struct inotify_event *event;
+    ssize_t len;
+    char *ptr;
+    pid_t newProcess;
+    char timeBuffer[32];
+    char operationBuffer[16];
+    char nameBuffer[1024];
+
+    // Loop while events can be read from inotify file descriptor
+    for(;;)
+    {
+        //Read some events
+        len = read(fd, buffer, sizeof(buffer));
+        if(len == -1 && errno != EAGAIN)
+        {
+            perror("Read error");
+            exit(EXIT_FAILURE);
+        }
+
+        if(len <= 0)
+            break;
+        
+        //Loop over all events in the buffer
+        for(ptr = buffer; ptr< buffer + len; ptr += sizeof(struct inotify_event) + event->len)
+        {
+            time_t currTime;
+            struct tm *timeInfo;
+            memset(nameBuffer, 0, 1024);
+            memset(operationBuffer, 0, 16);
+            event = (const struct inotify_event *)ptr;
+
+            if(!(event->mask & IN_OPEN))
+            {
+                memset(timeBuffer, 0, sizeof(timeBuffer));
+                currTime = time(NULL);
+                timeInfo = localtime(&currTime);
+                strftime(timeBuffer, 32, "%d-%B-%Y at %H:%M:%S", timeInfo);
+                write(fdHTML, timeBuffer, strlen(timeBuffer));
+                write(fdHTML, " -> ", strlen(" -> "));
+
+                if(event->mask & IN_CLOSE_WRITE)
+                    strcpy(operationBuffer, "WRITE: ");
+                if(event->mask & IN_CLOSE_NOWRITE)
+                    strcpy(operationBuffer, "READ: ");
+            }
+
+            write(fdHTML, operationBuffer, strlen(operationBuffer));
+
+            //Name of watched directory
+            if(wd == event->wd)
+                strcat(nameBuffer, dir);
+            
+            //File name
+            if(event->len)
+            {
+                write(fdHTML, event->name, strlen(event->name));
+                strcat(nameBuffer, event->name);
+            }
+
+            //Type of filesystem object
+            if(event->mask & IN_ISDIR)
+                write(fdHTML, " [dir]<br>", strlen(" [dir]<br>"));
+            else
+                write(fdHTML, " [file]<br>", strlen(" [file]<br>"));
+            
+            newProcess = fork();
+            if(newProcess == -1)
+                perror("Fork error");
+            
+            if(newProcess == 0)
+                sendToServer(timeBuffer, operationBuffer, nameBuffer);
+        }
+        
+    }
+}
 
 int cmd_backtrace(struct cli_def *cli, char *command, char *argv[], int argc)
 {
@@ -59,14 +179,14 @@ void *telnetBT()
 
     listenSkt = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(listenSkt, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
-
 }
 
 int main(int argc, char *argv[]) 
 {
-    int option,fdHTML,fd,wd;
+    int option,fdHTML,fd,wd,poll_num;
     pthread_t bt_thread;
     struct pollfd fds[2];
+    char buffer;
     sem_init(&semaphore, 0, 0);
 
     if(argc != 5)
@@ -143,4 +263,29 @@ int main(int argc, char *argv[])
     char *htmlHead = "<!DOCTYPE html><html><title>File Access Monitor</title><body>";
     write(fdHTML, htmlHead, strlen(htmlHead));
     printf("Listening for events\n");
+
+    while(1)
+    {
+        poll_num = poll(fds, nfds, -1);
+        if(poll_num == -1)
+        {
+            if(errno == EINTR)
+                continue;
+            perror("Poll error");
+            exit(EXIT_FAILURE);
+        }
+        if(poll_num > 0)
+        {
+            // Console input is available. Empty stdin and quit
+            while(read(STDIN_FILENO, &buffer, 1) > 0 && buffer != '\n')
+                continue;
+            break;
+        }
+
+        if(fds[1].revents & POLLIN)
+        {
+            //Inotify events are available
+            handle_events(fd, wd, fdHTML);
+        }
+    }
 }
